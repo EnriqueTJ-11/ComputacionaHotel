b38654b8527bbacf4612471d9c4558c46637d102
JaiderSaenz <jaidersaenz2002@gmail.com>
2025-05-21 00:47:50 -0500

    Registrar alojamiento terminado.

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '1128',
+  password: '0107',
   database: 'hotel_app',
   waitForConnections: true,
   connectionLimit: 10,
--- backend/routes/ontology.routes.js
+++ backend/routes/ontology.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { queryOntology } = require('../controllers/ontology.controller');
+const { queryOntology, createAlojamiento, getTiposAlojamiento } = require('../controllers/ontology.controller');
 
 const router = express.Router();
 
 // Rutas de ontología
 router.get('/query', queryOntology);
+router.post('/alojamiento', createAlojamiento); // Para insertar un alojamiento
+router.get('/tiposAlojamiento', getTiposAlojamiento); // Para obtener los tipos de alojamiento
 
 module.exports = router;
